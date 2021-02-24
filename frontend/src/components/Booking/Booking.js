6cf252941193a24a484b89e5fae12d207d24340e
Nick <nikhilmenon@comcast.net>
2021-02-24 08:18:51 -0600

    change

--- frontend/src/components/Booking/Booking.js
+++ frontend/src/components/Booking/Booking.js
@@ -6,13 +6,12 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { useDispatch, useSelector } from "react-redux";
 import Select from "react-select";
 import { useParams, useHistory } from "react-router-dom";
-import {fetch} from "../../store/csrf";
-import './Booking.css'
-
+import { fetch } from "../../store/csrf";
+import "./Booking.css";
 
 function Booking() {
   let { id } = useParams();
-  const user = useSelector((state) => state.session.user)
+  const user = useSelector((state) => state.session.user);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [people, setPeople] = useState({});
@@ -29,47 +28,42 @@ function Booking() {
     setEndDate(ranges.selection.endDate);
   }
 
+  const makeReservation = (e) => {
+    e.preventDefault();
 
-    const makeReservation = (e) => {
-      e.preventDefault();
-
-      if(user){
-        fetch("/api/bookings", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            guest_num: people.value,
-            startDate: startDate,
-            endDate: endDate,
-            userId: user.id,
-            spotId: id,
-          }),
-        });
-        return <h1>There are no reviews for this business yet!</h1>;
-      }
-      else{
-        history.push('/')
-        
-      }
+    if (user) {
+      fetch("/api/bookings", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          guest_num: people.value,
+          startDate: startDate,
+          endDate: endDate,
+          userId: user.id,
+          spotId: id,
+        }),
+      });
+      return <h1>There are no reviews for this business yet!</h1>;
+    } else {
+      history.push("/");
     }
-    
-
-
-
+  };
 
   return (
     <div className="search">
       <DateRangePicker
         ranges={[selectionRange]}
         onChange={handleSelect}
-        minDate ={new Date()}
+        minDate={new Date()}
       />
       <h2>Select Number Of Guest Below</h2>
       <GuestNum people={people} setPeople={setPeople} />
       <br></br>
-      <button className="nav-link" onClick={makeReservation}>Make Booking</button>
+      <button className="nav-link" onClick={makeReservation}>
+        Make Booking
+      </button>
     </div>
   );
 }
